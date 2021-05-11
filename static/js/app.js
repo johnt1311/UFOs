dd9602b15aa2696cb7b6f18da8b855e21b281e14
John Temple <JohnTemple@safeguardworld.com>
2021-05-11 17:44:24 -0500

    another update

--- static/js/app.js
+++ static/js/app.js
@@ -19,3 +19,22 @@ function buildTable(data) {
         });
     });
 }
+
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if a date was entered and filter the data using that date
+    if (date) {
+        // apply filter to the table data to only keep the rows where teh datetime value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // rebuild the table using the filtered data. if no date, it will show the original table
+    buildTable(filteredData)
+}
+
+// attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// build the table when the page loads
+buildTable(tableData);
